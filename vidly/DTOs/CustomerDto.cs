5c6d7b66644991f2cf9fc169c9ee184b8686859a
Uchenna <Uchenna.Umeh@dangote.com>
2019-01-28 13:29:00 +0100

    adding API folder for API, adding rest webService, using DTOs to decouple our domain models from Webservice obkects

---
+++ vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Customers Name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+        public byte MembershipTypeId { get; set; }
+
+   
+        [Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
