25d9db96d0236f7b43c8641800a45980ac1a22e7
Uchenna <Uchenna.Umeh@dangote.com>
2019-01-28 13:56:52 +0100

    configuring camel notations for our Api

--- vidly/App_Start/WebApiConfig.cs
+++ vidly/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace vidly
 {
@@ -9,6 +11,9 @@ namespace vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
