51c515e48541097779bbd27ef6b764b0554f7635
Uchenna <Uchenna.Umeh@dangote.com>
2019-01-30 16:41:43 +0100

    Adding Global "authorize', also allowing annonymous for home page

--- vidly/App_Start/FilterConfig.cs
+++ vidly/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace vidly
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- vidly/Controllers/HomeController.cs
+++ vidly/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace vidly.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
